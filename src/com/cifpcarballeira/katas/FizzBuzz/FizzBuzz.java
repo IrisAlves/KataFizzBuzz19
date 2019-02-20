c57049fcd4da6fe78790925eaa1b4794236839c6
IrisAlves <irisalvesfranco@gmail.com>
2019-02-20 11:52:29 +0100

    multiplo de 5 devuelve Buzz

--- src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
+++ src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -12,8 +12,11 @@ package com.cifpcarballeira.katas.FizzBuzz;
 public class FizzBuzz {
     
     public String getFizzBuzz(int i){
-        if (i%3==0) {
+        if (i%3==0){
             return "Fizz";
+        }
+        if (i%5==0) {
+            return "Buzz";
         }
         return i+"";
     }
