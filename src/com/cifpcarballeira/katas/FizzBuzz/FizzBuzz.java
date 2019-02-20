ccc3026adf8dfe6e36e444a7460b3b3ed594304a
IrisAlves <irisalvesfranco@gmail.com>
2019-02-20 10:22:40 +0100

    2 devuelve 2

--- src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
+++ src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -12,6 +12,9 @@ package com.cifpcarballeira.katas.FizzBuzz;
 public class FizzBuzz {
     
     public String getFizzBuzz(int i){
+        if (i==2) {
+           return "2"; 
+        }
         return "1";
     }
 }
